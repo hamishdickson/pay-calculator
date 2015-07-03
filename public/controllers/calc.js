0acb8f613390f80da63015d41bf9960f96f33e68
hamishdickson <hamish.dickson@gmail.com>
2015-07-03 21:10:49 +0100

    start using ng

--- public/controllers/calc.js
+++ public/controllers/calc.js
@@ -7,60 +7,60 @@
             restrict: 'E',
             templateUrl: 'pay-calculator/public/views/calc.html',
             controller: function () {
-                BASE_SALARY = 0.0;
-                BASE_SALARY_MONTH = BASE_SALARY / 12.0;
+                this.BASE_SALARY = 0.0;
+                this.BASE_SALARY_MONTH = BASE_SALARY / 12.0;
 
-                BONUS_RATE = 0.0;
+                this.BONUS_RATE = 0.0;
 
                 // tax fun
-                BASIC_RATE = 0.2;
-                HIGHER_RATE = 0.4;
-                ADDITIONAL_RATE = 0.45;
+                this.BASIC_RATE = 0.2;
+                this.HIGHER_RATE = 0.4;
+                this.ADDITIONAL_RATE = 0.45;
 
-                PERSONAL_ALLOWANCE = 10000.0;
-                BASIC_BAND = 31865.0 + PERSONAL_ALLOWANCE;
-                HIGHER_BAND = 150000.0;
+                this.PERSONAL_ALLOWANCE = 10000.0;
+                this.BASIC_BAND = 31865.0 + PERSONAL_ALLOWANCE;
+                this.HIGHER_BAND = 150000.0;
 
-                PERSONAL_ALLOWANCE_MONTH = PERSONAL_ALLOWANCE / 12.0;
-                BASIC_BAND_MONTH = BASIC_BAND / 12.0;
-                HIGHER_BAND_MONTH = HIGHER_BAND / 12.0;
+                this.PERSONAL_ALLOWANCE_MONTH = PERSONAL_ALLOWANCE / 12.0;
+                this.BASIC_BAND_MONTH = BASIC_BAND / 12.0;
+                this.HIGHER_BAND_MONTH = HIGHER_BAND / 12.0;
 
                 // student loan
-                STUDENT_LOAN_INTEREST_RATE = 0.015;
-                STUDENT_LOAN_RATE = 0.09;
-                STUDENT_LOAN_BAND = 16910.0;
-                STUDENT_LOAN_BAND_MONTH = STUDENT_LOAN_BAND / 12.0;
-                INITIAL_STUDENT_LOAN_LEFT = 0.0;
+                this.STUDENT_LOAN_INTEREST_RATE = 0.015;
+                this.STUDENT_LOAN_RATE = 0.09;
+                this.STUDENT_LOAN_BAND = 16910.0;
+                this.STUDENT_LOAN_BAND_MONTH = STUDENT_LOAN_BAND / 12.0;
+                this.INITIAL_STUDENT_LOAN_LEFT = 0.0;
 
-                _student_loan_paid = 0.0
+                this._student_loan_paid = 0.0
 
                 // national insurance
-                NATIONAL_INSURANCE_LOWER_LIMIT = 663.0;
-                NATIONAL_INSURANCE_UPPER_LIMIT = 3488.33;
-                NATIONAL_INSURANCE_LOWER_RATE = 0.12;
-                NATIONAL_INSURANCE_UPPER_RATE = 0.02;
+                this.NATIONAL_INSURANCE_LOWER_LIMIT = 663.0;
+                this.NATIONAL_INSURANCE_UPPER_LIMIT = 3488.33;
+                this.NATIONAL_INSURANCE_LOWER_RATE = 0.12;
+                this.NATIONAL_INSURANCE_UPPER_RATE = 0.02;
 
                 // pension
-                PENSION_RATE = 0.05;
-                PENSION_VALUE_MONTH = 0.0;
+                this.PENSION_RATE = 0.05;
+                this.PENSION_VALUE_MONTH = 0.0;
 
                 // outgoings
-                RENT_MONTH = 624.0;
+                this.RENT_MONTH = 624.0;
 
                 // months to consider
-                MONTHS = 12 * 1;
+                this.MONTHS = 12 * 1;
 
                 // savings - I actually save a bit more than this, but it's an easy approx for the future
-                SAVINGS_RATE = 0.25;
-                TYPICAL_SAVINGS = 0.0;
-                INITIAL_SAVINGS = 0.0;
+                this.SAVINGS_RATE = 0.25;
+                this.TYPICAL_SAVINGS = 0.0;
+                this.INITIAL_SAVINGS = 0.0;
 
-                ISA_INTEREST_RATE = 1.01;
+                this.ISA_INTEREST_RATE = 1.01;
 
-                __savings = [INITIAL_SAVINGS];
-                __savings_total = [];
-                __base = [];
-                __total_tax = [];
+                this.__savings = [INITIAL_SAVINGS];
+                this.__savings_total = [];
+                this.__base = [];
+                this.__total_tax = [];
 
 
 

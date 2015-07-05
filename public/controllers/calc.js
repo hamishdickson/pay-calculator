1619a87a05db5b82bebb94f9c76c29b733c06f20
hamishdickson <hamish.dickson@gmail.com>
2015-07-05 20:40:55 +0100

    initial graph

--- public/controllers/calc.js
+++ public/controllers/calc.js
@@ -28,20 +28,79 @@
                 var BASIC_BAND_MONTH = BASIC_BAND / 12.0;
                 var HIGHER_BAND_MONTH = HIGHER_BAND / 12.0;
 
+                var NATIONAL_INSURANCE_LOWER_LIMIT = 663.0;
+                var NATIONAL_INSURANCE_UPPER_LIMIT = 3488.33;
+                var NATIONAL_INSURANCE_LOWER_RATE = 0.12;
+                var NATIONAL_INSURANCE_UPPER_RATE = 0.02;
+
                 this.taxToPay = function() {
                     var out = 0.0;
-                    if (this.baseSalary > BASIC_BAND_MONTH) {
-                        out = (BASIC_BAND_MONTH - PERSONAL_ALLOWANCE_MONTH) * BASIC_RATE;
-                        out += (this.baseSalary - BASIC_BAND_MONTH) * HIGHER_RATE;
-                    } else if (this.baseSalary > PERSONAL_ALLOWANCE_MONTH) {
-                        out = (this.baseSalary - PERSONAL_ALLOWANCE_MONTH) * BASIC_RATE
+                    if (this.baseSalary > 0) {
+                        if (this.baseSalary > BASIC_BAND_MONTH) {
+                            out = (BASIC_BAND_MONTH - PERSONAL_ALLOWANCE_MONTH) * BASIC_RATE;
+                            out += (this.baseSalary - BASIC_BAND_MONTH) * HIGHER_RATE;
+                        } else if (this.baseSalary > PERSONAL_ALLOWANCE_MONTH) {
+                            out = (this.baseSalary - PERSONAL_ALLOWANCE_MONTH) * BASIC_RATE
+                        }
                     }
                     return out;
                 };
 
                 this.getMonthlyTax = function() {
                     return this.taxToPay() / 12;
-                }
+                };
+
+                this.nationalInsurance = function() {
+                    var out = 0.0;
+                    if (this.baseSalary > 0) {
+                        if (this.baseSalary > NATIONAL_INSURANCE_UPPER_LIMIT) {
+                            out = (NATIONAL_INSURANCE_UPPER_LIMIT - NATIONAL_INSURANCE_LOWER_LIMIT) * NATIONAL_INSURANCE_LOWER_RATE;
+                            out += (this.baseSalary - NATIONAL_INSURANCE_UPPER_LIMIT) * NATIONAL_INSURANCE_UPPER_RATE;
+                        } else {
+                            out = (this.baseSalary - NATIONAL_INSURANCE_LOWER_LIMIT) * NATIONAL_INSURANCE_LOWER_RATE
+                        }
+                    }
+                    return out;
+                };
+
+                this.payInPocket = function() {
+                    return this.baseSalary - this.taxToPay() - this.nationalInsurance();
+                };
+
+
+                this.payInPocketMonthly = function() {
+                    return this.payInPocket() / 12;
+                };
+
+
+
+                // pie chart
+                var ctx = document.getElementById("myChart").getContext("2d");
+
+                var data = [
+                    {
+                        value: 100,
+                        color:"#F7464A",
+                        highlight: "#FF5A5E",
+                        label: "Red"
+                    },
+                    {
+                        value: 55,
+                        color: "#46BFBD",
+                        highlight: "#5AD3D1",
+                        label: "Green"
+                    },
+                    {
+                        value: 100,
+                        color: "#FDB45C",
+                        highlight: "#FFC870",
+                        label: "Yellow"
+                    }
+                ];
+
+                new Chart(ctx).Doughnut(data, {
+                    animateScale: true
+                });
             },
             controllerAs: "calc"
         };
